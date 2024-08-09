3b4a827041e238f5607f15256aaa5b440547aaa2
Zixuan Chen <remch183@outlook.com>
2024-08-09 17:59:18 +0800

    chore: bench 10m sheet mem

--- crates/examples/examples/sheet.rs
+++ crates/examples/examples/sheet.rs
@@ -1,6 +1,17 @@
+use dev_utils::get_mem_usage;
 use examples::sheet::init_large_sheet;
+use loro::ID;
 
 pub fn main() {
-    let doc = init_large_sheet();
-    doc.export_snapshot();
+    let doc = init_large_sheet(10_000_000);
+    // let doc = init_large_sheet(10_000);
+    doc.commit();
+    let allocated = get_mem_usage();
+    println!("Allocated bytes for 10M cells spreadsheet: {}", allocated);
+
+    doc.checkout(&ID::new(doc.peer_id(), 100).into()).unwrap();
+
+    doc.checkout_to_latest();
+    let after_checkout = get_mem_usage();
+    println!("Allocated bytes after checkout: {}", after_checkout);
 }
--- crates/examples/src/sheet.rs
+++ crates/examples/src/sheet.rs
@@ -1,16 +1,15 @@
 use loro::{LoroDoc, LoroMap};
 
-pub fn init_large_sheet() -> LoroDoc {
+pub fn init_large_sheet(size: usize) -> LoroDoc {
+    assert!(size >= 100);
     let doc = LoroDoc::new();
     doc.set_peer_id(0).unwrap();
-    let cols = doc.get_list("cols");
     let rows = doc.get_list("rows");
-    for _ in 0..bench_utils::sheet::SheetAction::MAX_ROW {
-        rows.push_container(LoroMap::new()).unwrap();
-    }
-
-    for i in 0..bench_utils::sheet::SheetAction::MAX_COL {
-        cols.push(i as i32).unwrap();
+    for _ in 0..size / 10 {
+        let map = rows.push_container(LoroMap::new()).unwrap();
+        for i in 0..10 {
+            map.insert(&i.to_string(), i).unwrap();
+        }
     }
 
     doc
